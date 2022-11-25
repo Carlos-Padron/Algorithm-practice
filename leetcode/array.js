32c2596d1415d957e59bb25694e26bedeabfe601
Carlos Padrón <40147158+Carlos-Padron@users.noreply.github.com>
2022-11-24 21:13:52 -0500

    Update array.js

--- leetcode/array.js
+++ leetcode/array.js
@@ -73,3 +73,99 @@ var plusOne = function(digits) {
     return digits
     
 };
+
+
+
+//121. Best Time to Buy and Sell Stock
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+
+
+    let buy = 0
+    let sell = 1;
+    let max = 0
+    
+    
+    for(let i = 0; sell < prices.length; i++){
+        
+        if(prices[buy] < prices[sell]){
+            // Buy cheap, sell expensive
+            let tempProfit = prices[sell] - prices[buy] 
+            max = max < tempProfit? tempProfit : max;
+        }else{
+            buy = sell
+        }
+        sell++
+    }
+    
+    return max
+
+};
+
+
+
+//387. First Unique Character in a String
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    
+    //Hash to count the letters
+    let hash = {}
+    
+    for(let i = 0; i < s.length; i++ ){
+
+        //If it is not in the hash. 
+        if(!hash[s[i]]){
+            hash[s[i]] = 1
+        }else{
+            //If so, increase the counter
+            let counter = hash[s[i]]
+            hash[s[i]] = counter + 1
+        }
+    }
+
+    
+    //Iterate over the string
+    for(let i = 0; i < s.length; i++){
+        
+        //Check if the current letter exist in hash
+        // and has a value of 1 to return its index
+        if(hash[s[i]] == 1){
+            return i
+        }
+        
+    }
+    
+    
+    return -1;
+};
+
+
+
+//217. Contains Duplicate
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    
+    //hash to store the counter of each letter
+    let hash = {}
+    
+    for(let i = 0; i < nums.length; i++){
+        
+        if(!hash[nums[i]]){
+            hash[nums[i]] = 1
+        }else{
+            return true
+        }
+    }
+    
+    return false
+    
+};
